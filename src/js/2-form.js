4f5e125c588fa9dd525e7f68e26b4302241690fa
Bovdur1 <daratuz41@gmail.com>
2023-12-30 23:49:41 +0100

    Update form

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -4,20 +4,27 @@ const form = document.querySelector(".feedback-form");
 const localStorageKey = "feedback-data";
 
 // Initial data of the form
-if (localStorage.getItem(localStorageKey)) {
-    const localStorageData = JSON.parse(localStorage.getItem(localStorageKey));
-    if (localStorageData) {
+const formObject = {};
+const getLocalStorageData = localStorage.getItem(localStorageKey);
+
+if (getLocalStorageData !== null && getLocalStorageData !== undefined) {
+    try {
+       const localStorageData = JSON.parse(getLocalStorageData);
+
+        if (localStorageData) {
         form.elements.email.value = localStorageData.email;
         form.elements.message.value = localStorageData.message;
+            
+        formObject.email = localStorageData.email;
+        formObject.message = localStorageData.message;
     }
+    } catch (e) {}  
 }
-
-const formObject = {};
 
 // Saving form data to the local storage
 form.addEventListener("input", () => {
     const formData = new FormData(form);
-
+    
     formData.forEach((value, key) => {
         formObject[key] = value.trim();
     })
@@ -28,14 +35,15 @@ form.addEventListener("input", () => {
 // Verify and submit form data
 form.addEventListener("submit", (e) => {
     e.preventDefault()
+
     const email = e.currentTarget.elements.email.value;
     const message = e.currentTarget.elements.message.value;
-    
+
     if (email === "" || message === "") {
         alert('All form fields must be filled in');
     } else {
         console.log(formObject);
         form.reset()
         localStorage.removeItem(localStorageKey)
-    }
+    } 
 })
